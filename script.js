b59172053ded3c89857547e11353205212e96343
govea16 <49473134+govea16@users.noreply.github.com>
2025-04-28 10:12:18 -0600

    Saving calculator
    Added buttons and javascript for calculator functions

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log('HELLOO');
+function add(){
+    const nums = Array.from(arguments);
+    return nums.reduce((a,b) => a + b);
+}
+function subtract(){
+    const nums = Array.from(arguments);
+    return nums.reduce((a,b) => a-b);
+}
+function multiply(){
+    const nums = Array.from(arguments);
+    return nums.reduce((a,b) => a * b,1);
+}
+function divide(){
+    const nums = Array.from(arguments);
+    return nums.reduce((a,b) => a / b,);
+}
+
+function operate (a,b,sign) {
+    if (sign === '+') {
+        return add(a,b);
+    }
+    else if (sign === '-') {
+        return subtract(a,b);
+    }
+    else if (sign === '*') {
+        return multiply(a,b);
+    }
+    else if (sign === '/') {
+        return divide(a,b);
+    }
+    else {
+        return 'ERR!'
+    }
+}
